a57d0af231ed002d35657b2d72eabb73b5b03639
Emagh16@freeuni.edu.ge <Emagh16@freeuni.edu.ge>
2026-08-11 12:25:20 +0000

    Optimize exercise 6 with partitioning and clustering

--- models/models-marts/exercise_6_orders_with_segmentation_2026.sql
+++ models/models-marts/exercise_6_orders_with_segmentation_2026.sql
@@ -1,4 +1,14 @@
-{{ config(materialized='table') }}
+{{
+    config(
+        materialized='table',
+        partition_by={
+            "field": "order_date",
+            "data_type": "date",
+            "granularity": "month"
+        },
+        cluster_by=["customer_id"]
+    )
+}}
 
 with orders as (
 
